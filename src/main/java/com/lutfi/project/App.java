08ed6a8f09a9d8e9b625a4b5ce3ee03480cae424
Lutsapp <ls.lutsap@gmail.com>
2025-04-21 11:01:33 +0700

    initial commit with greeting task and dependencies

---
+++ src/main/java/com/lutfi/project/App.java
@@ -0,0 +1,10 @@
+package com.lutfi.project;
+
+import com.google.common.collect.ImmutableList;
+
+public class App {
+    public static void main(String[] args) {
+        ImmutableList<String> list = ImmutableList.of("Halo", "Gradle", "User");
+        System.out.println("List isi: " + list);
+    }
+}
